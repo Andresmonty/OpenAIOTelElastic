28174bba32f51667658fda24b761300ccf2a4ea5
Andres Montaño <andresmontano@Monty-MacBook-Pro.local>
2023-05-23 11:34:01 -0500

    ready baby

--- OpenTelemetryElastic.py
+++ OpenTelemetryElastic.py
@@ -3,6 +3,8 @@ from flask import Flask
 import monitor  # Import the module
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 import urllib
+import argparse
+import requests
 import os
 
 from opentelemetry import trace
@@ -14,7 +16,7 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 
 # Service name is required for most backends
 resource = Resource(attributes={
-    SERVICE_NAME: "your-service-name"
+    SERVICE_NAME: "OpenAIOpenTelemetryElastic"
 })
 
 provider = TracerProvider(resource=resource)
@@ -34,18 +36,18 @@ app = Flask(__name__)
 # Set OpenAI API key
 openai.api_key = os.getenv('OPEN_AI_KEY')
 
-@app.route("/completion")
+@app.route("/")
 @tracer.start_as_current_span("do_work")
 def completion():
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="Why is Elastic an amazing observability tool?",
-        max_tokens=20,
+        prompt=f"Act as a DevOps Engineer and Write a python script to automate the provisioning of an EC2 instance using terraform on AWS. Provide only code, no text",
+        max_tokens=75,
         temperature=0
     )
 
     return response.choices[0].text.strip()
-
+    return response.json()["choices"][0]["text"]
+    
 if __name__ == "__main__":
-    app.run(host="localhost", port=8000, debug=True)
-
+    app.run(host="localhost", port=8888, debug=False)
